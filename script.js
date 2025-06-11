47a0c05bfbef290358cf1094220900a615d8ddd1
raquelalfarom <alfaroraquel558@gmail.com>
2025-06-10 19:23:43 -0600

    fourth commit

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// envío del formulario
+
+const btn = document.getElementById("button");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  btn.value = "Sending...";
+
+  const serviceID = "default_service";
+  const templateID = "template_d3je9e5";
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = "Send Email";
+      alert("Sent!");
+
+      // Limpia los campos del formulario
+      form.reset();
+    },
+    (err) => {
+      btn.value = "Send Email";
+      alert(JSON.stringify(err));
+    }
+  );
+});
 // Mostrar y ocultar menú
 const menuIcon = document.querySelector("#menu-icon");
 const nav = document.querySelector("nav");
@@ -6,12 +33,3 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("fa-times");
   nav.classList.toggle("active");
 };
-
-// Detectar envío del formulario
-const form = document.querySelector("form");
-const submitButton = document.querySelector("form .btn");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault(); // importante prevenir el submit real si no tienes backend
-  submitButton.innerText = "Sending...";
-});
